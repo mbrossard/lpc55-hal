bb30966fb17e0dbd1d24da10ac12c41a0e81292c
Mathias Brossard <mathias@brossard.org>
2026-05-27 07:36:59 -0500

    lpc55-hal: bump to rtic 2 + usb-device 0.3 + usbd-serial 0.2
    - Cargo.toml: rtic 1 -> 2 (thumbv8main-backend), usb-device 0.2 -> 0.3, usbd-serial 0.1 -> 0.2
    - endpoint.rs: EndpointType::Isochronous is now struct variant -> matches!()
    - examples/rtic_led: rtic 2 syntax (Shared/Local, dispatchers)
    - examples/usb, usb_test_class: usb-device 0.3 StringDescriptors builder

--- examples/rtic_led.rs
+++ examples/rtic_led.rs
@@ -2,9 +2,8 @@
 #![no_std]
 
 extern crate panic_semihosting;
-// extern crate panic_halt;
 
-#[rtic::app(device = lpc55_hal::raw, peripherals = true)]
+#[rtic::app(device = lpc55_hal::raw, peripherals = true, dispatchers = [PIN_INT0])]
 mod app {
     use cortex_m_semihosting::dbg;
 
@@ -12,23 +11,20 @@ mod app {
     use lpc55_hal as hal;
 
     type RedLed = hal::Pin<pins::Pio1_6, pin::state::Gpio<pin::gpio::direction::Output>>;
+
     #[shared]
-    struct SharedResources {}
+    struct Shared {}
 
     #[local]
-    struct LocalResources {
+    struct Local {
         led: RedLed,
-        // delay: hal::clock::Ticks<'static, hal::syscon::Fro1MhzUtickClock<states::init_state::Enabled>>,
-        // sleep: hal::sleep::Busy<'static, 'static>,
     }
 
     #[init]
-    fn init(c: init::Context) -> (SharedResources, LocalResources, init::Monotonics) {
-        // dbg!("init");
+    fn init(c: init::Context) -> (Shared, Local) {
         let _cp = c.core;
         let dp = c.device;
 
-        // setup red LED
         let mut syscon = hal::Syscon::from(dp.SYSCON);
         let mut gpio = hal::Gpio::from(dp.GPIO).enabled(&mut syscon);
         let mut iocon = hal::Iocon::from(dp.IOCON).enabled(&mut syscon);
@@ -39,42 +35,18 @@ mod app {
             .into_gpio_pin(&mut iocon, &mut gpio)
             .into_output(Level::High);
 
-        // let clock = hal::syscon::Fro1MhzUtickClock::take()
-        //     .unwrap()
-        //     .enable(&mut syscon);
-        // // let clock = syscon.fro_1mhz_utick_clock.enable(&mut syscon.handle);
-        // let delay = hal::clock::Ticks {
-        //     value: 500_000,
-        //     clock: &clock,
-        // }; // 500 ms = 0.5 s
-
-        // // let mut utick = dp.UTICK.enable(&mut syscon.handle);
-        // let mut utick = hal::utick::wrap(dp.UTICK).enabled(&mut syscon, &clock);
-        // // let mut sleep = hal::sleep::Busy::prepare(&mut utick);
-        // let mut sleep = hal::sleep::Busy::wrap(&mut utick);
-        (
-            SharedResources {},
-            LocalResources {
-                led: red_led,
-                // delay,
-                // sleep,
-            },
-            init::Monotonics(),
-        )
+        (Shared {}, Local { led: red_led })
     }
 
-    // #[idle(resources = [led, delay, sleep])]
     #[idle(local = [led])]
     fn idle(ctx: idle::Context) -> ! {
         let led = ctx.local.led;
         loop {
             dbg!("low");
             led.set_low().unwrap();
-            // c.resources.sleep.sleep(c.resources.delay);
 
             dbg!("high");
             led.set_high().unwrap();
-            // c.resources.sleep.sleep(c.resources.delay);
         }
     }
 }
